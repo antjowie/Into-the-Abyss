245b41c6eb9d0abb8b3f84cb0f3458511fa8a73e
Angelo <7332321+antjowie@users.noreply.github.com>
2019-07-18 15:43:17 +0200

    Added placeholder assets and added camera Imported old characters and tilesets. Also configured the project settings and plugins. Made a very simple playground scene to show assets in action.

---
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float speed = 2f;
+
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = target.position - transform.position;
+        Vector2 position = transform.position;
+
+        rb.MovePosition(position + direction * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+    Rigidbody2D rb;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
