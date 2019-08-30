43c9730e11f02c032a44a0cc0a1aa57394953f9f
Angelo <7332321+antjowie@users.noreply.github.com>
2019-08-30 21:55:57 +0200

    current time is now updated while playing the game

--- Sources/Assets/Scripts/Menus/Timer.cs
+++ Sources/Assets/Scripts/Menus/Timer.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
+
+using System;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TextMeshProUGUI scoreText = null;
+    [SerializeField] TextMeshProUGUI highscoreText = null;
+
+    public float scoreTimer { private set; get; }
+
+    public bool shouldIncreaseScore = true;
+
+    private void Start()
     {
-        
+        if (!scoreText || !highscoreText)
+        {
+            Debug.Log("References not set in score text HUD. Timers aren't being updated!!!");
+            Destroy(this);
+        }
+        else
+        {
+            highscoreText.text = "NO HS";
+            scoreText.text = scoreTimer.ToString();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (shouldIncreaseScore)
+        {
+            scoreTimer += Time.deltaTime;
+            string span = TimeSpan.FromSeconds(scoreTimer).ToString(@"mm\:ss\.ff");
+            
+            scoreText.text = "CS " + span.ToString();
+        }
     }
 }
