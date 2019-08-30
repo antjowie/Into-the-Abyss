12356208d46a1eba2eeaefccec7e9d27fe430184
Angelo <7332321+antjowie@users.noreply.github.com>
2019-08-30 22:01:26 +0200

    Added a script that allows you to get the time the object exists since it was loaded

---
+++ Sources/Assets/Scripts/Utility/TimeSinceSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TimeSinceSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float initialTime;
+
+    void Start()
+    {
+        initialTime = Time.timeSinceLevelLoad;
+    }
+    
+    public float ElapsedTime()
+    {
+        return Time.timeSinceLevelLoad - initialTime;
+    }
+}
