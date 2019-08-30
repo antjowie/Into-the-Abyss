15cd264b3ecfbdadd898e4a4da5369b97ac77345
Angelo <7332321+antjowie@users.noreply.github.com>
2019-08-30 22:17:39 +0200

    Refactored timer script to be player only because we need to know when player dies and can easily do that by getting the player state

---
+++ Sources/Assets/Scripts/Characters/PlayerTimer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerTimer : MonoBehaviour
+{
+    // This is so that we can check the state to see if player is dead
+    PlayerController player = null;
+
+    float initialTime;
+    float deltaTime;
+
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+        deltaTime = initialTime = Time.timeSinceLevelLoad;
+        
+    }
+
+    private void Update()
+    {
+        if(player.state != PlayerController.PlayerState.Dead)
+            deltaTime = Time.timeSinceLevelLoad;
+    }
+
+    public float ElapsedTime()
+    {
+        return deltaTime - initialTime;
+    }
+}
---
+++ Sources/Assets/Scripts/Menus/ScoreHUD.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+using System;
+
+public class ScoreHUD : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText = null;
+    [SerializeField] TextMeshProUGUI highscoreText = null;
+    [SerializeField] PlayerTimer timer = null;
+
+    private void Start()
+    {
+        highscoreText.text = "NO HS";
+        ToUpdate();
+    }
+
+    private void Update()
+    {
+        ToUpdate();
+    }
+
+    // This function is called ToUpdate because it is supposed to be called during the update but also once
+    // during start. by doing this, I don't have to write logic twice
+    private void ToUpdate()
+    {
+        scoreText.text = "CS " + FormatToDate(timer.ElapsedTime());
+    }
+
+    private string FormatToDate(float aliveTimeInSeconds)
+    {
+        return TimeSpan.FromSeconds(aliveTimeInSeconds).ToString(@"mm\:ss\.ff");
+    }
+}
